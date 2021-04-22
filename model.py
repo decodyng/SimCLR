2cf72b941aaca0e1348738aee35b4746edf0aa80
Cody Wild <codywild@berkeley.edu>
2021-04-22 10:15:00 -0700

    Add an extra layer to mimic our architecture

--- model.py
+++ model.py
@@ -15,13 +15,14 @@ class Model(nn.Module):
             if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
                 self.f.append(module)
         # encoder
+        # Temporarily add an extra layer to be closer to our model implementation
+        self.f.append(nn.Linear(2048, 512, bias=False))
         self.f = nn.Sequential(*self.f)
         # projection head
-        self.g = nn.Sequential(nn.Linear(2048, 512, bias=False), nn.BatchNorm1d(512),
+        self.g = nn.Sequential(nn.Linear(512, 512, bias=False), nn.BatchNorm1d(512),
                                nn.ReLU(inplace=True), nn.Linear(512, feature_dim, bias=True))
 
     def forward(self, x):
-        breakpoint()
         x = self.f(x)
         feature = torch.flatten(x, start_dim=1)
         out = self.g(feature)
