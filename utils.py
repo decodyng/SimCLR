0ac8820acf32975708cb625691956ffa8c308755
Cody Wild <codywild@berkeley.edu>
2021-04-22 12:03:35 -0700

    Remove breakpoints that break workers

--- utils.py
+++ utils.py
@@ -11,13 +11,11 @@ class CIFAR10Pair(CIFAR10):
     def __getitem__(self, index):
         img, target = self.data[index], self.targets[index]
         img = Image.fromarray(img)
-        breakpoint()
         if index == 1:
             save_rgb_tensor(img, 'results/img_pre_trans.png')
         if self.transform is not None:
             pos_1 = self.transform(img)
             pos_2 = self.transform(img)
-        breakpoint()
         if self.target_transform is not None:
             target = self.target_transform(target)
 
