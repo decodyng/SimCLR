847313136ad592d733d36f7822975a1e2af8ecc1
Cody Wild <codywild@berkeley.edu>
2021-04-22 12:05:21 -0700

    okay, fine, always save out pre transformation

--- utils.py
+++ utils.py
@@ -11,8 +11,7 @@ class CIFAR10Pair(CIFAR10):
     def __getitem__(self, index):
         img, target = self.data[index], self.targets[index]
         img = Image.fromarray(img)
-        if index == 1:
-            save_rgb_tensor(img, 'results/img_pre_trans.png')
+        save_rgb_tensor(img, 'results/img_pre_trans.png')
         if self.transform is not None:
             pos_1 = self.transform(img)
             pos_2 = self.transform(img)
