28265b2a0a93957098a1d1c231cde3660e9ab87b
Cody Wild <codywild@berkeley.edu>
2021-04-22 14:01:05 -0700

    Remove image saves

--- utils.py
+++ utils.py
@@ -13,16 +13,12 @@ class CIFAR10Pair(CIFAR10):
         img, target = self.data[index], self.targets[index]
         img = Image.fromarray(img)
         id_val = np.random.randint(0, 50000)
-        print("Min")
-        print(np.min(img))
-        print("Max")
-        print(np.max(img))
-        save_image(img, f'results/{id_val}_img_pre_trans.png')
+        #save_image(img, f'results/{id_val}_img_pre_trans.png')
         if self.transform is not None:
             pos_1 = self.transform(img)
             pos_2 = self.transform(img)
-            save_rgb_tensor(pos_1, f'results/{id_val}_pos1.png')
-            save_rgb_tensor(pos_2, f'results/{id_val}_pos2.png')
+            # save_rgb_tensor(pos_1, f'results/{id_val}_pos1.png')
+            # save_rgb_tensor(pos_2, f'results/{id_val}_pos2.png')
         if self.target_transform is not None:
             target = self.target_transform(target)
 
